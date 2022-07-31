e80b4fe33d7f21a5e1087adbd31eac307a93a916
Sehyeon Park <nesmile23@gmail.com>
2022-08-01 03:26:04 +0900

    [feat]: 로그인, 로그아웃 기능 추가

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,11 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../redux/modules/user";
 
 function Header({ title }) {
+  const isLogin = useSelector((store) => store.user.isLogin);
+  const dispatch = useDispatch();
+
+  const handleLoginButton = () => {
+    dispatch(login());
+  };
+
+  const handleLogoutButton = () => {
+    dispatch(logout());
+  };
+
   return (
     <header css={headerStyle}>
       <h1>{title}</h1>
-      <button>로그인</button>
+      {/* <button onClick={isLogin ? handleLogoutButton : handleLoginButton}>
+        {isLogin ? "로그아웃" : "로그인"}
+      </button> */}
+      {isLogin ? (
+        <button onClick={handleLogoutButton}>로그아웃</button>
+      ) : (
+        <button onClick={handleLoginButton}>로그인</button>
+      )}
     </header>
   );
 }
